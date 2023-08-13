5b27447171e93264dd462395483ef4f8b39c7f81
Jubair Ahmed <jubairahmed3000@gmail.com>
2023-08-13 11:59:35 +0530

    ALL Changes

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import All from "./components/Categories-pages/All";
+import Furnitures from "./components/Categories-pages/Furnitures";
+import Electronics from "./components/Categories-pages/Electronics";
+import Lamps from "./components/Categories-pages/Lamps";
+import Kitchen from "./components/Categories-pages/Kitchen";
+import Chairs from "./components/Categories-pages/Chairs";
+import SkinCare from "./components/Categories-pages/SkinCare";
+import ProductPage, { CartContext } from "./pages/ProductPage";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cartItem, setCartItem] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItem([...cartItem, item]);
+  };
+
+  // local storage
+  useEffect(() => {
+    const json = localStorage.getItem("cartItem");
+    const savedCart = JSON.parse(json);
+    if (savedCart) {
+      setCartItem(savedCart);
+    }
+  }, []);
+
+  useEffect(() => {
+    const json = JSON.stringify(cartItem);
+    localStorage.setItem("cartItem", json);
+  }, [cartItem]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <CartContext.Provider value={{ cartItem, addToCart, setCartItem }}>
+      <Navbar />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+
+        <Route path="categories" element={<Categories />}>
+          <Route path="all" element={<All />} />
+          <Route path="furnitures" element={<Furnitures />} />
+          <Route path="electronics" element={<Electronics />} />
+          <Route path="lamps" element={<Lamps />} />
+          <Route path="kitchen" element={<Kitchen />} />
+          <Route path="chairs" element={<Chairs />} />
+          <Route path="skin-care" element={<SkinCare />} />
+        </Route>
+        <Route path="categories/product/:id" element={<ProductPage />} />
+      </Routes>
+    </CartContext.Provider>
+  );
 }
 
-export default App
+export default App;
